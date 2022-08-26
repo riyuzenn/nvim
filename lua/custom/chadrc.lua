558f49886ef9d6f57cbc8c7dce16b7dbc303b1d7
zenn <66673988+riyuzenn@users.noreply.github.com>
2022-08-26 17:20:42 +0200

    feat: add custom script

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,17 @@
+-- Just an example, supposed to be placed in /lua/custom/
+
+local M = {}
+
+-- make sure you maintain the structure of `core/default_config.lua` here,
+-- example of changing theme:
+
+M.plugins = {
+  user = require "custom.plugins",
+}
+
+M.ui = {
+  theme_toggle = { "mountain", "rosepine" },
+  theme = "mountain",
+}
+
+return M
